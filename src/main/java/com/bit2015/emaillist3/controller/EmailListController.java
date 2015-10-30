7fd035c1364b55296cb1812a3cd4b647711fb2e3
bit-user <bit-user@bit>
2015-10-30 17:57:52 +0900

    MyBatis 구현

---
+++ src/main/java/com/bit2015/emaillist3/controller/EmailListController.java
@@ -0,0 +1,41 @@
+package com.bit2015.emaillist3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bit2015.emaillist3.dao.EmailListDao;
+import com.bit2015.emaillist3.vo.EmailListVo;
+
+@Controller
+public class EmailListController {
+	@Autowired
+	@Qualifier("elDao")
+	EmailListDao emailListDao;
+
+	@RequestMapping("/list")
+	public String list(Model model) {
+		List<EmailListVo> list = emailListDao.getList();
+		model.addAttribute("list", list);
+		// model.addAttribute(list); // 첫번째 요소의 타입이름(클래스이름)에 List를 붙인다.
+		// emailListVoList
+		return "/board/list";
+	}
+
+	@RequestMapping("/form")
+	public String form() {
+		return "/board/form";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insert(@ModelAttribute EmailListVo vo) {
+		emailListDao.insert(vo);
+		return "redirect:/list";
+	}
+}
---
+++ src/main/java/com/bit2015/emaillist3/dao/EmailListDao.java
@@ -0,0 +1,23 @@
+package com.bit2015.emaillist3.dao;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.bit2015.emaillist3.vo.EmailListVo;
+
+@Repository("elDao")
+public class EmailListDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public void insert(EmailListVo vo) {
+		sqlSession.insert("emaillist.insert", vo);
+	}
+
+	public List<EmailListVo> getList() {
+		List<EmailListVo> list = sqlSession.selectList("emaillist.list2");
+		return list;
+	}
+}
